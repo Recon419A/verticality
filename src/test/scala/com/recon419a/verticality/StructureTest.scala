6c581d5cb8ca7bac2c9257a60a8d78ba4acbe9ce
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-15 17:39:48 -0600

    Add tests for translatedVoxelList.

--- src/test/scala/com/recon419a/verticality/StructureTest.scala
+++ src/test/scala/com/recon419a/verticality/StructureTest.scala
@@ -14,11 +14,21 @@ class StructureTest extends FlatSpec with Matchers {
   }
 
   "voxelList" should "list non-None voxels" in {
-    TestStructure.voxelList shouldBe List(Voxel(Coordinate(0, 0, 0), SimpleBlock.BRICK_BLOCK),
+    TestStructure.voxelList shouldBe List(
+      Voxel(Coordinate(0, 0, 0), SimpleBlock.BRICK_BLOCK),
       Voxel(Coordinate(0, 0, 1), SimpleBlock.BOOKSHELF),
       Voxel(Coordinate(0, 1, 0), SimpleBlock.CLAY),
       Voxel(Coordinate(1, 0, 0), SimpleBlock.COAL_BLOCK),
       Voxel(Coordinate(1, 1, 0), SimpleBlock.COBBLESTONE))
+  }
+
+  "translatedVoxelList" should "list non-None voxels with appropriate translation" in {
+    TestStructure.translatedVoxelList shouldBe List(
+      Voxel(Coordinate(0, 1, 1), SimpleBlock.BRICK_BLOCK),
+      Voxel(Coordinate(0, 1, 2), SimpleBlock.BOOKSHELF),
+      Voxel(Coordinate(0, 2, 1), SimpleBlock.CLAY),
+      Voxel(Coordinate(1, 1, 1), SimpleBlock.COAL_BLOCK),
+      Voxel(Coordinate(1, 2, 1), SimpleBlock.COBBLESTONE))
   }
 
   "contains" should "return true for a value in the box" in {
