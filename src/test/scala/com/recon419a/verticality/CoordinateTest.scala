05b1b8b99a1595e7251a37e714e32365564b0cd5
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-15 14:34:07 -0600

    Add coordinate tests.

---
+++ src/test/scala/com/recon419a/verticality/CoordinateTest.scala
@@ -0,0 +1,46 @@
+package com.recon419a.verticality
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CoordinateTest extends FlatSpec with Matchers {
+  "+" should "add two positive coordinates correctly" in {
+    Coordinate(1, 1, 1) + Coordinate(2, 3, 5) shouldBe Coordinate(3, 4, 6)
+  }
+
+  it should "handle negative arguments" in {
+    Coordinate(1, -2, 4) + Coordinate(-3, 5, -8) shouldBe Coordinate(-2, 3, -4)
+  }
+
+  "-" should "handle a basic case" in {
+    Coordinate(1, -3, 4) - Coordinate(2, -2, 8) shouldBe Coordinate(-1, -1, -4)
+  }
+
+  "<=" should "return true for a coordinate equal in all dimensions" in {
+    Coordinate(1, 2, 3) <= Coordinate(1, 2, 3) shouldBe true
+  }
+
+  it should "fail if any one dimension is higher" in {
+    Coordinate(0, 0, 1) <= Coordinate(0, 0, 0) shouldBe false
+    Coordinate(0, 1, 0) <= Coordinate(0, 0, 0) shouldBe false
+    Coordinate(1, 0, 0) <= Coordinate(0, 0, 0) shouldBe false
+  }
+
+  it should "fail if two dimensions are significantly lower and the third is higher" in {
+    Coordinate(0, 0, 1) <= Coordinate(100, 100, 0) shouldBe false
+  }
+
+  "to" should "produce an inclusive range along a single axis" in {
+    Coordinate(0, 0, 0) to Coordinate(0, 0, 1) shouldBe List(Coordinate(0, 0, 0), Coordinate(0, 0, 1))
+  }
+
+  it should "produce an inclusive range along two axes" in {
+    Coordinate(0, 0, 0) to Coordinate(0, 1, 1) shouldBe List(Coordinate(0, 0, 0), Coordinate(0, 0, 1),
+      Coordinate(0, 1, 0), Coordinate(0, 1, 1))
+  }
+
+  it should "produce an inclusive range along three axes" in {
+    Coordinate(0, 0, 0) to Coordinate(1, 1, 1) shouldBe List(Coordinate(0, 0, 0), Coordinate(0, 0, 1),
+      Coordinate(0, 1, 0), Coordinate(0, 1, 1), Coordinate(1, 0, 0), Coordinate(1, 0, 1), Coordinate(1, 1, 0),
+      Coordinate(1, 1, 1))
+  }
+}
