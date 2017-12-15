2d472cc8062f35ac61eb777143e48892aa7cd68e
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-15 16:33:05 -0600

    Implement MultiStructure.

---
+++ src/main/scala/com/recon419a/verticality/MultiStructure.scala
@@ -0,0 +1,16 @@
+package com.recon419a.verticality
+
+trait MultiStructure extends Structure {
+  val components: List[Structure]
+
+  /**
+    * A function defining the procedural shape of this multi-structure. The first of this structure's components which
+    * wishes to set a voxel is allowed to set that voxel; if none of the components sets that voxel, it will be None.
+    * This is the only voxel-defining function which needs to be overridden; the rest of the accessors depend upon it.
+    *
+    * @return a function mapping from coordinates to voxels
+    */
+  override def voxel: Coordinate => Option[Voxel] = {
+    coordinate => components.flatMap(_.voxel(coordinate)).headOption
+  }
+}
