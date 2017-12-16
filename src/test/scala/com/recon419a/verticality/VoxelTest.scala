40e75bdd2e951848a46e75b545fd99882dfdee67
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-15 18:36:32 -0600

    Cleanup unused function and imports.

--- src/test/scala/com/recon419a/verticality/VoxelTest.scala
+++ src/test/scala/com/recon419a/verticality/VoxelTest.scala
@@ -1,11 +1,10 @@
 package com.recon419a.verticality
 
 import net.morbz.minecraft.blocks.SimpleBlock
-import net.morbz.minecraft.level.{FlatGenerator, GameType, Level}
-import net.morbz.minecraft.world.{DefaultLayers, World}
-import org.scalatest.{FlatSpec, Matchers}
-import org.scalatest.mockito.MockitoSugar
+import net.morbz.minecraft.world.World
 import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
 
 class VoxelTest extends FlatSpec with Matchers with MockitoSugar {
   "offset" should "offset along the correct axes" in {
@@ -33,14 +32,5 @@ class VoxelTest extends FlatSpec with Matchers with MockitoSugar {
     val mockWorld = mock[World]
     Voxel(Coordinate(0, 1, -4), SimpleBlock.ACACIA_FENCE).renderTo(mockWorld)
     verify(mockWorld).setBlock(0, 1, -4, SimpleBlock.ACACIA_FENCE)
-  }
-
-  private def makeWorld = {
-    val layers = new DefaultLayers
-    val generator = new FlatGenerator(layers)
-    val level = new Level("Verticality", generator)
-    level.setGameType(GameType.CREATIVE)
-    level.setSpawnPoint(0, 0, 0)
-    new World(level, layers)
   }
 }
