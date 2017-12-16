d7d53a12ca5cba5f04dcbf6a951114fb9ea3ab99
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-15 18:34:54 -0600

    Add mocking to test rendering code.

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.12.4"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.4"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+libraryDependencies += "org.mockito" % "mockito-core" % "2.13.0" % "test"
 
         
--- src/test/scala/com/recon419a/verticality/VoxelTest.scala
+++ src/test/scala/com/recon419a/verticality/VoxelTest.scala
@@ -1,9 +1,13 @@
 package com.recon419a.verticality
 
 import net.morbz.minecraft.blocks.SimpleBlock
+import net.morbz.minecraft.level.{FlatGenerator, GameType, Level}
+import net.morbz.minecraft.world.{DefaultLayers, World}
 import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.mockito.MockitoSugar
+import org.mockito.Mockito._
 
-class VoxelTest extends FlatSpec with Matchers {
+class VoxelTest extends FlatSpec with Matchers with MockitoSugar {
   "offset" should "offset along the correct axes" in {
     Voxel(Coordinate(1, 2, 3), DEFAULT_MATERIAL).translate(Coordinate(4, 5, 6)) shouldBe
       Voxel(Coordinate(5, 7, 9), DEFAULT_MATERIAL)
@@ -17,5 +21,26 @@ class VoxelTest extends FlatSpec with Matchers {
   it should "work for non-default materials" in {
     Voxel(Coordinate(1, 1, 1), SimpleBlock.BRICK_BLOCK).translate(Coordinate(1, 2, 3)) shouldBe
       Voxel(Coordinate(2, 3, 4), SimpleBlock.BRICK_BLOCK)
+  }
+
+  "renderTo" should "call World.setBlock with the correct arguments" in {
+    val mockWorld = mock[World]
+    Voxel(Coordinate(0, 1, -4), DEFAULT_MATERIAL).renderTo(mockWorld)
+    verify(mockWorld).setBlock(0, 1, -4, DEFAULT_MATERIAL)
+  }
+
+  it should "work for non-default materials" in {
+    val mockWorld = mock[World]
+    Voxel(Coordinate(0, 1, -4), SimpleBlock.ACACIA_FENCE).renderTo(mockWorld)
+    verify(mockWorld).setBlock(0, 1, -4, SimpleBlock.ACACIA_FENCE)
+  }
+
+  private def makeWorld = {
+    val layers = new DefaultLayers
+    val generator = new FlatGenerator(layers)
+    val level = new Level("Verticality", generator)
+    level.setGameType(GameType.CREATIVE)
+    level.setSpawnPoint(0, 0, 0)
+    new World(level, layers)
   }
 }
