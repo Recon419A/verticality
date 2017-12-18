85623a58624c7181fb4fcf2e926c52f66c707fa5
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-18 16:53:48 -0600

    Add tests for and debug FilledCuboid.

--- src/main/scala/com/recon419a/verticality/structures/FilledCuboid.scala
+++ src/main/scala/com/recon419a/verticality/structures/FilledCuboid.scala
@@ -1,7 +1,17 @@
 package com.recon419a.verticality.structures
 
-import com.recon419a.verticality.{Coordinate, DEFAULT_MATERIAL, Structure, Voxel}
+import com.recon419a.verticality.{Coordinate, ORIGIN, DEFAULT_MATERIAL, Structure, Voxel}
 
-case class FilledCuboid(maxCoordinate: Coordinate, offset: Coordinate) extends Structure {
-  override def voxel: Coordinate => Option[Voxel] = { c => Some(Voxel(c, DEFAULT_MATERIAL)) }
+class FilledCuboid(val maxCoordinate: Coordinate, val offset: Coordinate) extends Structure {
+  override def voxel: Coordinate => Option[Voxel] = c => {
+    if ((ORIGIN to maxCoordinate).contains(c)) {
+      Some(Voxel(c, DEFAULT_MATERIAL))
+    } else None
+  }
+}
+
+object FilledCuboid {
+  def apply(maxCoordinate: Coordinate, offset: Coordinate): FilledCuboid = {
+    new FilledCuboid(maxCoordinate, offset)
+  }
 }
