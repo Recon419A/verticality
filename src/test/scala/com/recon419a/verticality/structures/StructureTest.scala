14e1fa95cf31f2baa936528a7ea1b82525021937
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-20 19:02:02 -0600

    Add tests for structure addition.

--- src/test/scala/com/recon419a/verticality/structures/StructureTest.scala
+++ src/test/scala/com/recon419a/verticality/structures/StructureTest.scala
@@ -1,6 +1,6 @@
 package com.recon419a.verticality.structures
 
-import com.recon419a.verticality.util.{Coordinate, Voxel}
+import com.recon419a.verticality.util.{Coordinate, Size, Voxel}
 import com.recon419a.verticality.util.Constants.DEFAULT_MATERIAL
 import net.morbz.minecraft.blocks.SimpleBlock
 import net.morbz.minecraft.world.World
@@ -8,13 +8,26 @@ import org.mockito.Mockito.verify
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
 
-class StructureTest extends FlatSpec with Matchers  with MockitoSugar {
+class StructureTest extends FlatSpec with Matchers with MockitoSugar {
   "+" should "correctly translate a single voxel" in {
     (Cuboid(1, 1, 1) + Coordinate(1, 2, 3)).voxels shouldBe Set(Voxel(1, 2, 3))
   }
 
   it should "correctly translate two voxels" in {
     (Cuboid(2, 1, 1) + Coordinate(1, 2, 3)).voxels shouldBe Set(Voxel(1, 2, 3), Voxel(2, 2, 3))
+  }
+
+  it should "add two voxels together" in {
+    (Cuboid(1, 1) + (Cuboid(1, 1) + Coordinate(1, 0))) shouldBe Cuboid(2, 1)
+  }
+
+  it should "add two overlapping 2x1's together into a 3x1" in {
+    (Cuboid(2, 1) + (Cuboid(2, 1) + Coordinate(1, 0))) shouldBe Cuboid(3, 1)
+  }
+
+  it should "use materials from the second structure in case of conflict" in {
+    (Cuboid(2, 1) + (Cuboid(2, 1, SimpleBlock.ACACIA_FENCE) + Coordinate(1, 0))).voxel(1, 0, 0) shouldBe
+      Some(Voxel(1, 0, 0, SimpleBlock.ACACIA_FENCE))
   }
 
   "renderTo" should "call setBlock with the correct arguments" in {
