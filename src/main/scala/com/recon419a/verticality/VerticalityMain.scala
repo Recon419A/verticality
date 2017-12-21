9e76e538d6499628b623d9278f27ceace4106c90
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-20 20:48:58 -0600

    Demonstrate igloos.

--- src/main/scala/com/recon419a/verticality/VerticalityMain.scala
+++ src/main/scala/com/recon419a/verticality/VerticalityMain.scala
@@ -1,6 +1,6 @@
 package com.recon419a.verticality
 
-import com.recon419a.verticality.structures.{Cuboid, Grid, Hallway}
+import com.recon419a.verticality.structures.{Cuboid, Grid, Hallway, Igloo}
 import com.recon419a.verticality.util.{Coordinate, Size}
 import net.morbz.minecraft.blocks.SimpleBlock
 import net.morbz.minecraft.level.{FlatGenerator, GameType, Level}
@@ -13,6 +13,7 @@ object VerticalityMain extends App {
 
   (Hallway(Size(5, 5, 8)) + Coordinate(0, 10)).renderTo(world)
   (Grid(Size(5, 5, 8), Coordinate(2, 2, 2), SimpleBlock.GLOWSTONE) + Coordinate(0, 10)).renderTo(world)
+  (Igloo(Size(9, 8, 9), Size(5, 5, 8)) + Coordinate(0, 20)).renderTo(world)
 
 
   private def makeFloor(): Unit = {
