cf135a21c9b21042751b7002f49843b57d330374
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-29 20:53:52 -0600

    Wrong structure; walled hallways in correct shape.

--- src/main/scala/com/recon419a/verticality/structures/AtriumRingLocations.scala
+++ src/main/scala/com/recon419a/verticality/structures/AtriumRingLocations.scala
@@ -2,10 +2,10 @@ package com.recon419a.verticality.structures
 
 import com.recon419a.verticality.util.{Coordinate, GridCoordinate}
 
-object GridCoordinateRing {
+object AtriumRingLocations {
   def apply(radius: Int): IndexedSeq[Coordinate] = {
     for (x <- -radius to radius;
          z <- -radius to radius
-         if Math.abs(x) == radius || Math.abs(z) == radius) yield GridCoordinate(x, 0, z)
+         if (Math.abs(x) == radius || Math.abs(z) == radius) && x % 2 == 0 && z % 2 == 0) yield GridCoordinate(x, 0, z)
   }
 }
--- src/main/scala/com/recon419a/verticality/structures/Dungeon.scala
+++ src/main/scala/com/recon419a/verticality/structures/Dungeon.scala
@@ -3,8 +3,8 @@ package com.recon419a.verticality.structures
 object Dungeon {
   def apply(): Structure = {
     val center = ClosedAtrium()
-    val ring1 = GridCoordinateRing(2).map(OpenAtrium() + _)
-    val ring2 = GridCoordinateRing(4).map(ClosedAtrium() + _)
+    val ring1 = AtriumRingLocations(2).map(OpenAtrium() + _)
+    val ring2 = AtriumRingLocations(4).map(ClosedAtrium() + _)
     center ++ ring2 ++ ring1
   }
 }
