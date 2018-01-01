8dab5fe3f42bce9562328b9b8bf148f548141b9b
Donald Bronson <donald.a.bronson@gmail.com>
2017-12-31 18:41:26 -0600

    Correct structure; blocked hallways.

---
+++ src/main/scala/com/recon419a/verticality/structures/AtriumRing.scala
@@ -0,0 +1,35 @@
+package com.recon419a.verticality.structures
+
+import com.recon419a.verticality.util.{Coordinate, GridCoordinate}
+
+object AtriumRing {
+  def apply(radius: Int): IndexedSeq[Structure] = {
+    val grid = for (x <- -4 * radius to 4 * radius;
+                    z <- -4 * radius to 4 * radius)
+      yield GridCoordinate(x, 0, z)
+    val openAtriums = grid.filter(isOpenAtriumLocation(_, radius)).map(OpenAtrium() + _)
+    val closedAtriums = grid.filter(isClosedAtriumLocation(_, radius)).map(ClosedAtrium() + _)
+    closedAtriums ++ openAtriums
+  }
+
+  def isClosedAtriumLocation(c: Coordinate, radius: Int): Boolean = {
+    isMixedDiamond(radius) && c.x % 4 == 0 && c.z % 4 == 0
+  }
+
+  def isOpenAtriumLocation(c: Coordinate, radius: Int): Boolean = {
+    (isOpenOnlyDiamond(radius) && onDiamond(c, radius) && c.x % 2 == 0 && c.z % 2 == 0) ||
+      (isMixedDiamond(radius) && onDiamond(c, radius) && c.x % 4 == 2 && c.z % 4 == 2)
+  }
+
+  def isOpenOnlyDiamond(radius: Int) = {
+    radius % 2 == 1
+  }
+
+  def isMixedDiamond(radius: Int) = {
+    radius % 2 == 0
+  }
+
+  def onDiamond(c: Coordinate, radius: Int): Boolean = {
+    Math.abs(c.z - c.x) == 4 * radius || Math.abs(c.z + c.x) == 4 * radius
+  }
+}
--- src/main/scala/com/recon419a/verticality/structures/Dungeon.scala
+++ src/main/scala/com/recon419a/verticality/structures/Dungeon.scala
@@ -2,9 +2,6 @@ package com.recon419a.verticality.structures
 
 object Dungeon {
   def apply(): Structure = {
-    val center = ClosedAtrium()
-    val ring1 = AtriumRingLocations(2).map(OpenAtrium() + _)
-    val ring2 = AtriumRingLocations(4).map(ClosedAtrium() + _)
-    center ++ ring2 ++ ring1
+    ClosedAtrium() ++ AtriumRing(1) ++ AtriumRing(2)
   }
 }
