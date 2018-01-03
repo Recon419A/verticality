a66b44d50d51e6977f81809acea74f8e1c1189b6
Donald Bronson <donald.a.bronson@gmail.com>
2018-01-03 15:57:47 -0600

    Add tests for apply for Atrium.

--- src/test/scala/com/recon419a/verticality/structures/AtriumTest.scala
+++ src/test/scala/com/recon419a/verticality/structures/AtriumTest.scala
@@ -4,7 +4,7 @@ import com.recon419a.verticality.util.{GridCoordinate, Size}
 import org.scalatest.{FlatSpec, Matchers}
 
 class AtriumTest extends FlatSpec with Matchers {
-  private val testWing = Cuboid(Size(1, 1))
+  private val testWing = Cuboid(Size(1, 1, 2))
 
   "actualWings" should "contain no wings when passed all false" in {
     TestAtrium.actualWings(Seq(false, false, false, false)) shouldBe List.empty[Structure]
@@ -38,6 +38,28 @@ class AtriumTest extends FlatSpec with Matchers {
   it should "correctly generate a west wing" in {
     TestAtrium.actualWings(Seq(false, false, false, true)).head shouldBe (
       testWing.rotateClockwise + GridCoordinate(-1, 0))
+  }
+
+  "apply" should "generate a standard room if all wings are set to false" in {
+    TestAtrium(Seq(false, false, false, false)) shouldBe Room()
+  }
+
+  it should "generate a Room with four additional blocks if all wings are true" in {
+    TestAtrium(Seq(true, true, true, true)).voxels.size shouldBe Room().voxels.size + 4
+  }
+
+  it should "generate a room with three additional blocks if three wings are true" in {
+    TestAtrium(Seq(false, true, true, true)).voxels.size shouldBe Room().voxels.size + 3
+  }
+
+  it should "add the wings to the room" in {
+    TestAtrium(Seq(true, false, true, false)) shouldBe (
+      Room() ++ TestAtrium.actualWings(Seq(true, false, true, false)))
+  }
+
+  it should "add different wings to the room" in {
+    TestAtrium(Seq(true, true, false, false)) shouldBe (
+      Room() ++ TestAtrium.actualWings(Seq(true, true, false, false)))
   }
 
   object TestAtrium extends Atrium {
