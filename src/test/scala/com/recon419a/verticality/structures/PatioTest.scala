46d53964922337be431dc3ed3feba93c10d6dce9
Donald Bronson <donald.a.bronson@gmail.com>
2018-01-04 15:55:26 -0600

    Add test for Patio.

--- src/test/scala/com/recon419a/verticality/structures/PatioTest.scala
+++ src/test/scala/com/recon419a/verticality/structures/PatioTest.scala
@@ -1,6 +1,7 @@
 package com.recon419a.verticality.structures
 
 import com.recon419a.verticality.util.Voxel
+import net.morbz.minecraft.blocks.SimpleBlock
 import org.scalatest.{FlatSpec, Matchers}
 
 class PatioTest extends FlatSpec with Matchers {
@@ -10,5 +11,9 @@ class PatioTest extends FlatSpec with Matchers {
 
   it should "create a structure with no voxels on the first layer" in {
     Patio().voxels.filter(_.coordinate.y == 0) shouldBe Set.empty[Voxel]
+  }
+
+  it should "create a structure with air on the inside" in {
+    Patio().voxels should contain allOf(Voxel(4, 1, 15, SimpleBlock.AIR), Voxel(6, 1, 13, SimpleBlock.AIR))
   }
 }
